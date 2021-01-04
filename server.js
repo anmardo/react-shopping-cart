1d7ea23f595051c064264e91a6c8ae6b676bda9f
Amu <am.martinez.domingo@gmail.com>
2021-01-04 20:37:31 +0100

    deploy-heroku-first-commit

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
+//prod build version
+app.use("/", express.static(__dirname + "/build"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
 //mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
-mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const Product = mongoose.model(
   "products",
